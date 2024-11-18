914315fbc0fcfbd65620e6f4bb051c0492a8a11b
JuanArchila13 <juanpabloarchila2003@outlook.com>
2024-11-18 12:03:21 -0500

    Changes in constants

--- front_gestion_caf/src/constants/constants.js
+++ front_gestion_caf/src/constants/constants.js
@@ -6,36 +6,34 @@ export const USER_TYPE = {
     
 };
 
-const BASE_URL = "https://api-gateway-caf.onrender.com"
-
 export const SERVICES_BACK = {
-    GOOGLELOGIN: BASE_URL + "/auth/google/login",
-    LOGINAUTH: BASE_URL + '/auth/login',
-    LOGOUTAUTH: BASE_URL + '/auth/logout ',
-    USERAUTH: BASE_URL + '/auth/verify/user',
-    PASSWORDAUTH: BASE_URL + '/auth/change/password',
-    CREATEAUTH: BASE_URL + '/auth/create',
-    VALIDATEAUTH: BASE_URL + '/auth/validate',
-    SAVEUSER: BASE_URL + '/user/save',
-    GETALLQUESTIONS: BASE_URL + '/caf/question/all',
-    GET_USER_ACTIVE_INSCRIPTION: BASE_URL + '/caf/inscription/all/',
+    GOOGLELOGIN: "https://api-gateway-caf.onrender.com/auth/google/login",
+    LOGINAUTH: 'https://api-gateway-caf.onrender.com/auth/login',
+    LOGOUTAUTH: 'https://api-gateway-caf.onrender.com/auth/logout ',
+    USERAUTH: 'https://api-gateway-caf.onrender.com/auth/verify/user',
+    PASSWORDAUTH: 'https://api-gateway-caf.onrender.com/auth/change/password',
+    CREATEAUTH: 'https://api-gateway-caf.onrender.com/auth/create',
+    VALIDATEAUTH: 'https://api-gateway-caf.onrender.com/auth/validate',
+    SAVEUSER: 'https://api-gateway-caf.onrender.com/user/save',
+    GETALLQUESTIONS: 'https://api-gateway-caf.onrender.com/caf/question/all',
+    GET_USER_ACTIVE_INSCRIPTION: 'https://api-gateway-caf.onrender.com/caf/inscription/all/',
 
-    GET_IS_USER_VERIFIED: 'http://localhost:9093/auth/isUserVerified/',
-    GET_DEPARTMETS: 'http://localhost:9093/user/departments',
-    GET_PROGRAMS: 'http://localhost:9093/user/programs' ,
-    GET__ALL_CAF: 'http://localhost:9093/caf/all' ,
-    GET_USET_ALL_CAF: 'http://localhost:9093/caf/inscription/all/' ,
-    GET_IS_USER_OLD_MAYOR: 'http://localhost:9093/user/isUserOldMayor/',
-    GET_SHIFT_LIST: 'http://localhost:9093/shift/allDayAssignments/',
-    GET_USER_ALL: 'http://localhost:9093/auth/user/all',
-    GET_USER_INSTANCE_SHIFT: 'http://localhost:9093/shift/reserve/shift-instances-caf/',
+    GET_IS_USER_VERIFIED: 'https://api-gateway-caf.onrender.com/auth/isUserVerified/',
+    GET_DEPARTMETS: 'https://api-gateway-caf.onrender.com/user/departments',
+    GET_PROGRAMS: 'https://api-gateway-caf.onrender.com/user/programs' ,
+    GET__ALL_CAF: 'https://api-gateway-caf.onrender.com/caf/all' ,
+    GET_USET_ALL_CAF: 'https://api-gateway-caf.onrender.com/caf/inscription/all/' ,
+    GET_IS_USER_OLD_MAYOR: 'https://api-gateway-caf.onrender.com/user/isUserOldMayor/',
+    GET_SHIFT_LIST: 'https://api-gateway-caf.onrender.com/shift/allDayAssignments/',
+    GET_USER_ALL: 'https://api-gateway-caf.onrender.com/auth/user/all',
+    GET_USER_INSTANCE_SHIFT: 'https://api-gateway-caf.onrender.com/shift/reserve/shift-instances-caf/',
     
 
-    POST_CAF_INSCRIPTION: 'http://localhost:9093/caf/inscription/inscribe-user/',
-    POST_CONSENT_FILE: "http://localhost:9093/caf/files/upload/",
-    POST_SAVE_SHIFT: "http://localhost:9093/shift/saveShift",
-    POST_INSTANCE_SHIFT: "http://localhost:9093/shift/reserve/create-shift-instances/",
-    PUT_DELETE_SHIFT: "http://localhost:9093/shift/deleteShift",
+    POST_CAF_INSCRIPTION: 'https://api-gateway-caf.onrender.com/caf/inscription/inscribe-user/',
+    POST_CONSENT_FILE: "https://api-gateway-caf.onrender.com/caf/files/upload/",
+    POST_SAVE_SHIFT: "https://api-gateway-caf.onrender.com/shift/saveShift",
+    POST_INSTANCE_SHIFT: "https://api-gateway-caf.onrender.com/shift/reserve/create-shift-instances/",
+    PUT_DELETE_SHIFT: "https://api-gateway-caf.onrender.com/shift/deleteShift",
 
 }
 
