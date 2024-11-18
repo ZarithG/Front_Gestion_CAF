fc503a92e041a92d714450e85f637f0287af699c
JuanArchila13 <juanpabloarchila2003@outlook.com>
2024-11-18 11:53:58 -0500

    Updated routes

--- front_gestion_caf/src/constants/constants.js
+++ front_gestion_caf/src/constants/constants.js
@@ -6,29 +6,31 @@ export const USER_TYPE = {
     
 };
 
+const BASE_URL = "https://api-gateway-caf.onrender.com"
+
 export const SERVICES_BACK = {
-    GOOGLELOGIN: "http://localhost:9093/auth/google/login",
-    LOGINAUTH: 'http://localhost:9093/auth/login',
-    LOGOUTAUTH: 'http://localhost:9093/auth/logout ',
-    USERAUTH: 'http://localhost:9093/auth/verify/user',
-    PASSWORDAUTH: 'http://localhost:9093/auth/change/password',
-    CREATEAUTH: 'http://localhost:9093/auth/create',
-    VALIDATEAUTH: 'http://localhost:9093/auth/validate',
-    SAVEUSER: 'http://localhost:9093/user/save',
-    GETALLQUESTIONS: 'http://localhost:9093/caf/question/all',
-    GET_USER_ACTIVE_INSCRIPTION: 'http://localhost:9093/caf/inscription/all/',
+    GOOGLELOGIN: BASE_URL + "/auth/google/login",
+    LOGINAUTH: BASE_URL + '/auth/login',
+    LOGOUTAUTH: BASE_URL + '/auth/logout ',
+    USERAUTH: BASE_URL + '/auth/verify/user',
+    PASSWORDAUTH: BASE_URL + '/auth/change/password',
+    CREATEAUTH: BASE_URL + '/auth/create',
+    VALIDATEAUTH: BASE_URL + '/auth/validate',
+    SAVEUSER: BASE_URL + '/user/save',
+    GETALLQUESTIONS: BASE_URL + '/caf/question/all',
+    GET_USER_ACTIVE_INSCRIPTION: BASE_URL + '/caf/inscription/all/',
 
-    GET_IS_USER_VERIFIED: 'http://localhost:9093/auth/isUserVerified/',
-    GET_DEPARTMETS: 'http://localhost:9093/user/departments',
-    GET_PROGRAMS: 'http://localhost:9093/user/programs' ,
-    GET__ALL_CAF: 'http://localhost:9093/caf/all' ,
-    GET_IS_USER_OLD_MAYOR: 'http://localhost:9093/user/isUserOldMayor/',
-    GET_SHIFT_LIST: 'http://localhost:9093/shift/allDayAssignments/',
+    GET_IS_USER_VERIFIED: BASE_URL + '/auth/isUserVerified/',
+    GET_DEPARTMETS: BASE_URL + '/user/departments',
+    GET_PROGRAMS: BASE_URL + '/user/programs' ,
+    GET__ALL_CAF: BASE_URL + '/caf/all' ,
+    GET_IS_USER_OLD_MAYOR: BASE_URL + '/user/isUserOldMayor/',
+    GET_SHIFT_LIST: BASE_URL + '/shift/allDayAssignments/',
 
 
-    POST_CAF_INSCRIPTION: 'http://localhost:9093/caf/inscription/inscribe-user/',
-    POST_CONSENT_FILE: "http://localhost:9093/caf/files/upload/",
-    POST_SAVE_SHIFT: "http://localhost:9093/shift/saveShift"
+    POST_CAF_INSCRIPTION: BASE_URL + '/caf/inscription/inscribe-user/',
+    POST_CONSENT_FILE: BASE_URL + '/caf/files/upload/',
+    POST_SAVE_SHIFT: BASE_URL + '/shift/saveShift'
 }
 
 export const STATUS = {
