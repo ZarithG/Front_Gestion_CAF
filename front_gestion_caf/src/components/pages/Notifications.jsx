6f2f4041aa88686dd5949d5b9528deab034fb12a
Zarith G <zarith.gomez@uptc.edu.co>
2024-11-27 13:49:28 -0500

    CoordinatorManage

--- front_gestion_caf/src/components/pages/Notifications.jsx
+++ front_gestion_caf/src/components/pages/Notifications.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Navigate, useNavigate } from "react-router-dom";
 import { USER_TYPE } from "../../constants/constants";
 import "../styles/Register.css"
 
+const initialUser = {
+    code: "", fullName: " ", email: "ejemplo@email.com", status: ""
+};
+
 const Notifications = () => {
-    const userName = localStorage.getItem("userName");
+    const [user, setUser] = useState(initialUser);
+    //const userName = localStorage.getItem("userName");
     return (
-    <div className="rfConteiner">
-        <h1> Usuario {userName}</h1>
-        <div className="rfRegister">
-            <h2 className="InformationDataPageTitle">CAF inscripto</h2>
-            <label></label>
+        <div className="rfConteiner">
+            <h1> Usuario {user.email}</h1>
+            <div>
+                <div className="rfRegister">
+                    <h2 className="InformationDataPageTitle">CAF inscripto</h2>
+                    <label></label>
+                </div>
+                <div className="rfRegister">
+                    <h2 className="InformationDataPageTitle">Fecha de inscripción</h2>
+                    <label></label>
+                </div>
+                <div className="rfRegister">
+                    <h2 className="InformationDataPageTitle">Estado de inscripción</h2>
+                    <label></label>
+                </div>
+            </div>
+            <div>
+                <div className="rfRegister">
+                    <h2 className="InformationDataPageTitle">Estado de inscripción</h2>
+                    <label>Hola</label>
+                </div>
+            </div>
+            <div>
+                <h2>Turnos</h2>
+                <label> </label>
+            </div>
         </div>
-        <div className="rfRegister">
-            <h2 className="InformationDataPageTitle">Fecha de inscripción</h2>
-            <label></label>
-        </div>
-        <div className="rfRegister">
-            <h2 className="InformationDataPageTitle">Estado de inscripción</h2>
-            <label></label>
-        </div>
-    </div>
     )
 };
 
--- front_gestion_caf/src/components/pages/pagesAdmin/ManageFitnessCenters.jsx
+++ front_gestion_caf/src/components/pages/pagesAdmin/ManageFitnessCenters.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { showToastPromise } from "../../gestion-caf/Messages"; // Asegúrate de importar correctamente
+import { IoMdSearch } from "react-icons/io";
 import "./styles/FormAdm.css";
+import { SERVICES_BACK } from "../../../constants/constants";
+
+const initialUsers = [
+    { code: "", fullName: " ", email: "", status: "" },
+];
 
 const ManageFitnessCenters = () => {
+    const [users, setUsers] = useState(initialUsers);
+    const [search, setSearch] = useState("");
+    const [error, setError] = useState("");
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
         name: "",
@@ -21,6 +30,15 @@ const ManageFitnessCenters = () => {
     });
     const [errors, setErrors] = useState({});
     const [submitted, setSubmitted] = useState(false);
+
+    const handleSearch = (event) => setSearch(event.target.value);
+
+    const filteredUsers = users.filter(
+        (user) =>
+            user.code.toLowerCase().includes(search.toLowerCase()) ||
+            user.name.toLowerCase().includes(search.toLowerCase()) ||
+            user.lastname.toLowerCase().includes(search.toLowerCase())
+    );
 
     const validateForm = () => {
         const newErrors = {};
@@ -41,6 +59,52 @@ const ManageFitnessCenters = () => {
 
         return newErrors;
     };
+
+    useEffect(() => {
+        const fetchCUserAll = async () => {
+            const token = localStorage.getItem("authToken");
+
+            const fetchUsers = async () => {
+                const response = await fetch(SERVICES_BACK.GET_USER_ALL, {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        credentials: "include",
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error("Error al obtener los datos de los usuarios");
+                }
+
+                const data = await response.json();
+                console.log(data)
+                if (Array.isArray(data)) {
+                    // Procesar los datos al formato deseado
+                    return data.map((user) => ({
+                        code: user.id.toString(), // Convertir el ID a una cadena
+                        fullName: user.name, // Usar el nombre completo del objeto
+                        email: user.userName, // Usar el correo como email
+                        status: user.active ? "Activo" : "Inactivo", // Convertir boolean a texto
+                    }));
+                } else {
+                    throw new Error("El formato de datos de CAF es incorrecto.");
+                }
+            };
+
+            try {
+                await showToastPromise(
+                    fetchUsers().then((processedUsers) => setUsers(processedUsers)),
+                    "Datos cargados correctamente",
+                    "Error al cargar los datos"
+                );
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchCUserAll();
+    }, []); // Dependencias vacías para ejecutar solo al montar
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -80,43 +144,141 @@ const ManageFitnessCenters = () => {
     return (
         <div className="containerBody">
             <h1 className="InformationDataPageTitleFormAdm">Datos Centro de Acondicionamiento Físico</h1>
-            
+
             {/* Información personal */}
             <div className="containerPersonalInformationFormAdm">
                 <div className="containerForm">
-                    <h2 className="h2FormAdm">Información del CAF</h2>
-                    <p className="pFormAdm">Datos de información</p>
-                    <form className="info-form" onSubmit={handleSubmit}>
-                        <div className="informationCAFMaFiCe">
-                            <div className="form-group-FormAdmHo">
-                                <label className="lbFormAdm">Nombre</label>
-                                <input
-                                    className="inMaFi"
-                                    type="text"
-                                    name="name"
-                                    value={formData.name}
-                                    onChange={handleChange}
-                                />
-                                {submitted && errors.name && <span className="error">{errors.name}</span>}
+                    <div>
+                        <h2 className="h2FormAdm">Información del CAF</h2>
+                        <p className="pFormAdm">Datos de información</p>
+                        <form className="info-form" onSubmit={handleSubmit}>
+                            <div className="informationCAFMaFiCe">
+                                <div className="form-group-FormAdmHo">
+                                    <label className="lbFormAdm">Nombre</label>
+                                    <input
+                                        className="inMaFi"
+                                        type="text"
+                                        name="name"
+                                        value={formData.name}
+                                        readOnly
+                                    />
+                                    {submitted && errors.name && <span className="error">{errors.name}</span>}
+                                </div>
+                                <div className="form-group-FormAdmHo">
+                                    <label className="lbFormAdm">Descripción</label>
+                                    <input
+                                        className="inMaFi"
+                                        type="text"
+                                        name="name"
+                                        value={formData.name}
+                                        readOnly
+                                    />
+                                    {submitted && errors.name && <span className="error">{errors.name}</span>}
+                                </div>
+                                <div className="form-group-FormAdmHo">
+                                    <label className="lbFormAdm">Número de Usuarios inscritos</label>
+                                    <input
+                                        type="text"
+                                        name="numberOfUsers"
+                                        value={formData.lastName}
+                                        readOnly
+                                    />
+                                    {submitted && errors.lastName && <span className="error">{errors.lastName}</span>}
+                                </div>
                             </div>
-                            <div className="form-group-FormAdmHo">
-                                <label className="lbFormAdm">Número de Usuarios inscritos</label>
-                                <input
-                                    type="text"
-                                    name="numberOfUsers"
-                                    value={formData.lastName}
-                                    onChange={handleChange}
-                                />
-                                {submitted && errors.lastName && <span className="error">{errors.lastName}</span>}
-                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <h2>Coordinador</h2>
+                        <div className="form-group-FormAdmHo">
+                            <label className="lbFormAdm">Nombre</label>
+                            <input
+                                className="inMaFi"
+                                type="text"
+                                name="name"
+                                value={formData.name}
+                                readOnly
+                            />
+                            {submitted && errors.name && <span className="error">{errors.name}</span>}
                         </div>
-                        {/* Más campos... */}
-                        <button type="submit" className="submit-button">Enviar</button>
-                    </form>
+                        <div className="form-group-FormAdmHo">
+                            <label className="lbFormAdm">Correo</label>
+                            <input
+                                className="inMaFi"
+                                type="text"
+                                name="name"
+                                value={formData.name}
+                                readOnly
+                            />
+                            {submitted && errors.name && <span className="error">{errors.name}</span>}
+                        </div>
+                        <button onClick={handleChangeCoordinador}>Cambiar Coordinador</button>
+                    </div>
+                    {/* Más campos... */}
+                    <div className="body-containerBody">
+                        <SearchBar search={search} handleSearch={handleSearch} />
+                        <div className="table-content">
+                            <UserTable users={filteredUsers} />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
+};
+
+
+const SearchBar = ({ search, handleSearch }) => (
+    <div className="containerSearch">
+        <div className="search-bar-field">
+            <label className="lbInItem">Ingrese el código o el número de documento del usuario</label>
+            <div className="search-bar-field-icon">
+                <input
+                    type="search"
+                    value={search}
+                    onChange={handleSearch}
+                    placeholder="Buscar usuario"
+                    className="search-bar-input"
+                />
+                <IoMdSearch className="search-icon" />
+            </div>
+        </div>
+
+    </div>
+);
+
+const UserTable = ({ users }) => (
+    <table className="table">
+        <thead className="table-header-head">
+            <tr className="table-row">
+                <th className="table-cell">Código</th>
+                <th className="table-cell">Nombre Completo</th>
+                <th className="table-cell">Correo</th>
+                <th className="table-cell">Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users.map((user, index) => (
+                <UserTableRow key={`${user.code}-${index}`} user={user} />
+            ))}
+        </tbody>
+    </table>
+);
+
+const UserTableRow = ({ user }) => (
+    <tr className="table-row">
+        <td className="table-cell">{user.code}</td>
+        <td className="table-cell">{user.fullName}</td>
+        <td className="table-cell">{user.email}</td>
+        <td className="table-cell">
+            <Status status={user.status} />
+        </td>
+    </tr>
+);
+
+const Status = ({ status }) => {
+    const statusClass = status.toLowerCase() === "activo" ? "status-active" : "status-inactive";
+    return <span className={`status ${statusClass}`}>{status}</span>;
 };
 
 export default ManageFitnessCenters;
